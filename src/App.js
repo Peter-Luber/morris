b0a85283371ab1c434061042998c3ee57363f9e6
Peter Luber <prluber@gmail.com>
2020-02-28 23:55:11 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import helados from "./midi/pissypamper.mp3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let audio = new Audio(helados);
+
+class App extends React.Component {
+  state = {
+    playing: "▶️",
+    track: ""
+  };
+
+  play = (song, title) => {
+    switch (this.state.playing) {
+      case "▶️":
+        song.play();
+        this.setState({ playing: "⏸", track: title });
+        break;
+      case "⏸":
+        song.pause();
+        this.setState({ playing: "▶️", track: title });
+
+        break;
+      default:
+        console.log("fuck");
+    }
+  };
+
+  picToggle = () => {
+    switch (this.state.playing) {
+      case "▶️":
+        return "https://521dimensions.com/img/open-source/amplitudejs/examples/single-song/play.svg";
+      case "⏸":
+        return "https://521dimensions.com/img/open-source/amplitudejs/examples/single-song/pause.svg";
+      default:
+        break;
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Player">
+          <span
+            className="Player-header"
+            onClick={() => console.log(this.picToggle())}
+          >
+            pop 5
+          </span>
+          <div id="playBox">
+            <img
+              id="controlButton"
+              alt="null"
+              src={this.picToggle()}
+              onClick={() =>
+                this.play(audio, "playboi carti - pissy pamper ft. young nudy")
+              }
+              playing="paused"
+            ></img>
+            <div
+              id="emojiButton"
+              playing="paused"
+              onClick={() =>
+                this.play(audio, "playboi carti - pissy pamper ft. young nudy")
+              }
+              text={this.state.playing}
+            >
+              {this.state.playing}
+            </div>
+          </div>
+          <span className="Current-track">{this.state.track}</span>
+        </div>
+        <div className="Player">
+          <span className="Player-header">pop 4</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
