eea9caca02d93e514c79a0e305b5672d10e34083
Peter Luber <prluber@gmail.com>
2020-03-04 12:51:24 -0800

    still working on player

--- src/App.js
+++ src/App.js
@@ -2,46 +2,62 @@ import React from "react";
 import "./App.css";
 import playButton from "./img/play.svg";
 import pauseButton from "./img/pause.svg";
-import carti from "./midi/pissypamper.mp3";
+import nudy from "./midi/pissypamper.mp3";
 import uplifter from "./midi/uplifter.mp3";
 
 class App extends React.Component {
   state = {
-    playing: "▶️",
-    track: ""
+    button: playButton,
+    title: "pop 5",
+    current: 0,
+    pop5: [
+      {
+        title: "lex luger riser",
+        url: uplifter
+      },
+      {
+        title: "young nudy - pissy pamper ft. playboi carti",
+        url: nudy
+      }
+    ]
   };
 
-  currentTrack = () => {
-    return uplifter;
-  };
-
-  play = (song, title) => {
-    switch (this.state.playing) {
-      case "▶️":
-        song.play();
-        this.setState({ playing: "⏸", track: title });
-        console.log("console");
-        break;
-      case "⏸":
-        song.pause();
-        this.setState({ playing: "▶️", track: title });
-        break;
-      default:
-        console.log("fuck");
-    }
-  };
-
-  picToggle = () => {
-    while (this.state.playing === "▶️") {
-      return playButton;
-    }
-    while (this.state.playing === "⏸") {
-      return pauseButton;
+  playPause = song => {
+    if (this.state.button === playButton) {
+      song.play();
+      this.setState({
+        button: pauseButton,
+        title: this.state.pop5[this.state.current].title
+      });
+    } else if (this.state.button === pauseButton && !this.refs.audioRef.ended) {
+      song.pause();
+      this.setState({
+        button: playButton
+      });
+    } else if (this.state.button === pauseButton && this.refs.audioRef.ended) {
+      song.play();
     }
   };
 
   trackEnd = () => {
-    this.setState({ playing: "▶️", track: "" });
+    if (this.refs.audioRef.ended) {
+      this.setState({
+        title: this.state.pop5[this.state.current + 1].title,
+        current: this.state.current + 1
+      });
+    } else {
+      this.setState({
+        button: playButton,
+        title: ""
+      });
+    }
+  };
+
+  endCycle = () => {
+    this.trackEnd();
+    console.log(this.refs.audioRef.ended);
+    console.log(this.refs.audioRef);
+    this.refs.audioRef.play();
   };
 
   render() {
@@ -49,9 +65,8 @@ class App extends React.Component {
       <div className="App">
         <audio
           ref="audioRef"
-          src={this.currentTrack()}
-          id="carti"
-          onEnded={() => this.trackEnd()}
+          src={this.state.pop5[this.state.current].url}
+          onEnded={() => this.endCycle()}
         ></audio>
         <div className="Player">
           <span className="Player-header">POP 5</span>
@@ -59,14 +74,15 @@ class App extends React.Component {
             <img
               id="controlButton"
               alt="play/pause button"
-              src={this.picToggle()}
-              onClick={() => this.play(this.refs.audioRef, this.currentTrack())}
+              src={this.state.button}
+              onClick={() => this.playPause(this.refs.audioRef)}
             ></img>
           </div>
-          <span className="Current-track">{this.state.track}</span>
-        </div>
-        <div className="Player">
-          <span className="Player-header">pop 4</span>
+          <span className="Current-track">
+            {this.state.current + 1}
+            <br></br>
+            {this.state.title}
+          </span>
         </div>
       </div>
     );
